9dc12ae66a5697387b65a20435ebfe75f5d38b56
songuijun <125720533+songuijun@users.noreply.github.com>
2024-01-12 20:18:52 +0900

    Publisher.cpp

--- 240112/gugudan/src/Publisher.cpp
+++ 240112/gugudan/src/Publisher.cpp
@@ -1,23 +1,31 @@
 #include "ros/ros.h"
-#include "std_msgs/Int32.h"
+#include "std_msgs/String.h"
+#include <sstream>
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "gugudan_publisher");
   ros::NodeHandle n;
-  ros::Publisher gugudan_pub = n.advertise<std_msgs::Int32>("gugudan", 1000);
+  ros::Publisher gugudan_pub = n.advertise<std_msgs::String>("gugudan", 1000);
   ros::Rate loop_rate(1);
 
-  std_msgs::Int32 msg;
-
-  for (int i = 1; i <= 9; i++)
+  while (ros::ok())
   {
-    for (int j = 1; j <= 9; j++)
-    {
-      msg.data = i * j;
-      gugudan_pub.publish(msg);
-    }
+    std_msgs::String msg;
 
+    std::stringstream ss;
+    for (int i = 1; i <= 9; i++)
+    {
+      for (int j = 1; j <= 9; j++)
+      {
+		ss << "\n";
+        ss << i << " * " << j << " = " << (i * j) << "\n";
+      }
+      ss << "\n";
+    }
+    msg.data = ss.str();
+    ROS_INFO("%s", msg.data.c_str());
+    gugudan_pub.publish(msg);
     ros::spinOnce();
     loop_rate.sleep();
   }
